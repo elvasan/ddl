6654453b9e0bfd81222201b1100a91d14bf9b3e1
John Osborne <josborne@jornaya.com>
2017-10-27 13:30:35 -0400

    add script to convert visual paradigm to hive ddl

---
+++ convert_vp_to_hive_ddl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+#
+# convert_vp_to_hive_ddl.sh
+#
+# Translates Visual Paradigm generated DDL files into environment agnostic
+# appliable DDL files.
+
+for ddl_file in `find . -name "*.ddl"`; do
+  sed -i '' 's/CREATE TABLE/CREATE EXTERNAL TABLE/g' $ddl_file
+  sed -i '' 's/NOT NULL,/,/g' $ddl_file
+  sed -i '' 's/ char{/ varchar{/g' $ddl_file
+  sed -i '' 's/PRIMARY KEY (node_key))/)/g' $ddl_file
+  sed -i '' 's/int2/smallint/g' $ddl_file
+  sed -i '' 's/int4/int/g' $ddl_file
+  sed -i '' 's/int8/bigint/g' $ddl_file
+  sed -i '' "s/-dev-/-${ENV}-/g" $ddl_file
+done
